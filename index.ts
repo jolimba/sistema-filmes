f17da1b227c3ddf8011c3194fca7ee769a222caa
jolimba <joovictors85@gmail.com>
2022-08-28 15:31:50 -0300

    cors tentativa 2

--- index.ts
+++ index.ts
@@ -1,15 +1,12 @@
 import express, {Application, Request, Response} from 'express'
 const app: Application = express()
+const cors = require('cors')
 const port = process.env.PORT || 3333
 var bodyParser = require('body-parser')
 var user = require('./config/user.route')
 var userMiddleware = require('./src/middleware/UserMiddleware')
 
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000/"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-})
+app.use(cors())
 app.use(bodyParser.json())
 
 app.post('/users', userMiddleware.verifyBodyUser, user.addNewUser)
